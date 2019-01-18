13c8d93d277164d852714f7268955cfd4d5af83c
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-01-18 15:16:21 +0100

    AGAIN

--- LCD.py
+++ LCD.py
@@ -29,11 +29,11 @@ def setRGB(r,g,b):
     bus.write_byte_data(DISPLAY_RGB_ADDR,3,g)
     bus.write_byte_data(DISPLAY_RGB_ADDR,2,b)
 
-# send command to display (no need for external use)
+# send command to display (no need for external use)    
 def textCommand(cmd):
     bus.write_byte_data(DISPLAY_TEXT_ADDR,0x80,cmd)
 
-# set display text \n for second line(or auto wrap)
+# set display text \n for second line(or auto wrap)     
 def setText(text):
     textCommand(0x01) # clear display
     time.sleep(.05)
@@ -79,7 +79,7 @@ def setText_norefresh(text):
 
 ##### FONCTION NOUVELLE #####
 def action(temps,nbrOption=1):
-    # Potentiomètre
+    # Potentiomètre    
     posStart=grovepi.analogRead(0)
     mvtPosition=0
     # Boutons
@@ -88,7 +88,7 @@ def action(temps,nbrOption=1):
     # Temps
     tStart=time.time()
     tCourant=time.time()
-
+   
     while (tCourant < tStart+temps) and (mvtPosition == 0) and not bouton1 and not bouton2:
         # Potentiomètre
         posCourante=grovepi.analogRead(0)
@@ -99,15 +99,15 @@ def action(temps,nbrOption=1):
         # Bouton 1
         if grovepi.digitalRead(3)==1:
             bouton1=True
-
+ 
 	# Bouton 2
         if grovepi.digitalRead(2)==1:
             bouton2=True
-        tCourant=time.time()
+        tCourant=time.time()   
     return mvtPosition,bouton1,bouton2
+    
 
-
-#def afficherLCD(text,rgb=[0,255,0]):
+def afficherLCD(text,rgb=[0,255,0]):
 #"""
 #Données :
 #    text (Chaine de caractères) que lon souhaite afficher sur lécran lcd
@@ -119,84 +119,21 @@ def action(temps,nbrOption=1):
 #"""
 
     ### Initialise la couleur de fond
-    #setRGB(rgb[0],rgb[1],rgb[2])
+    setRGB(rgb[0],rgb[1],rgb[2])
 
     ### Analyse le texte pour ne pas couper de mot
-    #reste=len(text)%16
-    #nbrLigne=len(text)//16+1
+    reste=len(text)%16
+    nbrLigne=len(text)//16+1
 
-    #i=1
+    i=1
 #    while i<nbrLigne :
 #        if (text[i*16-1] != " ") and (text[i*16-1] != ",") and (text[i*16-1] != "!") and (text[i*16-1] != "?") and (text[i*16-1] != ":") and (text[i*16] != " "):
 #            while caractere != " "
 
 
 
-def afficherLCD(text,rgb=[0,255,0],nbrOption=0,ignorer=[False,False]):
-    """
-    Données :
-        text (Chaine de caractères) que l'on souhaite afficher sur lécran lcd
-
-            Options :
-            rgb (Liste de int E {0;255}) pour définir la couleur de fond d'écran
-            Spécifique à action :
-            nbrOption (int>=0) pour le menu déroulant, si 0 alors pas de menu déroulant donc potentiomètre inactif
-            ignorer (Liste de bool) pour rendre les boutons inactifs
-
-        Résultat :
-        Affiche le texte sur l'écran LCD
-        Lorsque l'utilisateur agit sur un bouton/potentiomètre actif, alors la fonction prend fin et renvoie des informations sur l'action"""
-
-    ### Initialise la couleur de fond
-    setRGB(rgb[0],rgb[1],rgb[2])
-
-    ### Analyse du texte
-    reste=len(text)%16
-    nbrLigne=len(text)//16+1
-
-    i=1 # Itérateur de ligne
-    while i<nbrLigne:
-
-        ## Si un mot est coupé en 2
-        if (text[i*16-1] != " ") and (text[i*16-1] != ",") and (text[i*16-1] != "!") and (text[i*16-1] != "?") and (text[i*16-1] != ":") and (text[i*16] != " "):
-            decalage=1 # Invariant
-            caractere=text[i*16-1-decalage]
-
-            # Comptons le nombre de caractère à décaler à la ligne
-            while (caractere != " ") and (decalage < 15):
-                decalage+=1
-                caractere=text[i*16-1-decalage]
-
-            # Cas Particulier : Le mot est plus long que 16 caractères
-            if (decalage == 15) and ((caractere != " ") or (text[i*16+1] != " ")):
-                text=text[:i*16-1]+"-"+text[i*16-1:]
-                nbrLigne=nbrLigne+(reste+1)//16
-                reste=(reste+1)%16
-
-            # Cas Usuel : On décale le mot à la ligne
-            else :
-                text=text[:i*16-decalage]+"\n"+text[i*16-decalage:]
-                nbrLigne=nbrLigne+(reste+decalage)//16
-                reste=(reste+decalage)%16
-        i+=1
-
-
-    ### Affiche le texte et fait défiler si nécessaire jusqu'à ce que l'utilisateur agisse
-    pot,bouton1,bouton2=0,False,False
-    # On évalue action mais pas la première fois -> Do...While
-    while not pot and not bouton1 and not bouton2:
-        i=0 # Itérateur de ligne
-        while i<nbrLigne and not pot and not bouton1 and not bouton2:
-            lignes=text[i*16:(i+2)*16] # Caractères 0 à 31 etc
-            setText(lignes)
-            i+=1
-            pot,bouton1,bouton2=action(2,nbrOption,ignorer)
-
-    return pot,bouton1,bouton2
-
-
     ### Affiche le texte et fait défiler si nécessaire jusqu'à ce que quelqu'un agisse
-    a = False
+    a = False	
     while not a:
                      # On évalue action mais pas la première fois -> Do...While
         i=0 # Itérateur de ligne
@@ -205,7 +142,7 @@ def afficherLCD(text,rgb=[0,255,0],nbrOption=0,ignorer=[False,False]):
             ligne1=text[i*16:(i+2)*16] # Caractères 0 à 31 etc
             if i==nbrLigne-1: # S'il n'y a pas de ligne i+2
                 ligne1=text[i*16:(i+1)*16+reste]
-
+	                   
             setText(ligne1)
             i+=1
             a,b,c = action(3)
--- menu_test.py
+++ menu_test.py
@@ -1,24 +1,21 @@
-from LCD import *
-from classes import *
-import grovepi
-from Fonctions import *
 from test_lcd import *
+from EcranLCD import *
+from classes import *
+from Fonctions import *
 color1 = [255,0,0]
 color2 = [0,255,0]
 color3 = [0,0,255]
-print("ok1")
-option1 = Option("Premier choix", color1, 0)
-option2 = Option("Second choix", color2,0)
-option3 = Option("Troisieme choix", color3, 0)
-print("ok2")
+option1 = Option("J aime", color1, 0)
+option2 = Option("Les", color2,0)
+option3 = Option("musees", color3, 0)
 quit = False
 Options = [option1,option2,option3]
-num_current_option = grovepi.analogRead(0)//3
-print("ok3")
+num_current_option = analogRead(0)//3
+if num_current_option > 2:
+	num_current_option = 2
 while not quit:
-	print("ok4")
-	action_potentiometre, action_bouton1, action_bouton2 = afficherLCD(Options[num_current_option].name,Options[num_current_option].color)
-	print("ok5")
+	action_potentiometre, action_bouton1, action_bouton2 = afficherLCD(Options[num_current_option].name,Options[num_current_option].color,3)
+	print(action_potentiometre)
 	if action_potentiometre == 1 and num_current_option != len(Options) - 1:
 		num_current_option += 1
 	elif action_potentiometre == -1 and num_current_option != 0:
@@ -27,3 +24,4 @@ while not quit:
 		quit = True
 
 
+afficherLCD("Au revoir poto")
