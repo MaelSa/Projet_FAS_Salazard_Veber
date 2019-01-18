46378a87a243b17eac79d58e20a269e2c43665b9
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-01-18 19:34:30 +0100

    change main

--- EcranLCD.py
+++ EcranLCD.py
@@ -122,12 +122,19 @@ def afficherLCD(text,rgb=[0,255,0],nbrOption=0,ignorer=[False,False]):
 #Fonction permettant d'afficher un menu composé des options dans la liste d'options donnée en argument
 def menu_options(Options):
 
-
+	#color1 = [255,0,0]
+	#color2 = [0,255,0]
+	#color3 = [0,0,255]
+	#option1 = Option("J aime", color1, 0)
+	#option2 = Option("Les", color2,0)
+	#option3 = Option("musees", color3, 0)
+	#quit = False
+	#Options = [option1,option2,option3]
 	num_current_option = analogRead(0)//len(Options)
 	if num_current_option > len(Options) - 1:
 		num_current_option = len(Options) - 1
 	while not quit:
-		action_potentiometre, action_bouton1, action_bouton2 = afficherLCD(Options[num_current_option].name,Options[num_current_option].color,len(Options))
+		action_potentiometre, action_bouton1, action_bouton2 = afficherLCD(Options[num_current_option].name,Options[num_current_option].color,len())
 		print(action_potentiometre)
 		if action_potentiometre == 1 and num_current_option != len(Options) - 1:
 			num_current_option += 1
--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from Options import *
 
 Mode_1_joueur = Option("Mode 1 joueur", [0,0,0], "")
 Mode_2_joueurs = Option("Mode 2 joueurs", [0,0,0], "")
+fin_de_programme = False
+while not fin_de_programme:
+    Option_choisie = menu_options([Mode_1_joueur, Mode_2_joueurs])
 
-Option_choisie = menu_options([Mode_1_joueur, Mode_2_joueurs])
 
+    if Option_choisie == 1:#On lance le mode 1 joueur
+        changer_mode = False
+        while not changer_mode
+            choix_quiz1 = Option("Quiz1",[0,0,0], "")
+            choix_quiz2 = Option("Quiz2",[0,0,0], "")
+            choix_quiz3 = Option("Quiz3",[0,0,0], "")
+            menu_options([choix_quiz1,choix_quiz2,choix_quiz3])
 
-if Option_choisie == 1:#On lance le mode 1 joueur
-        choix_quiz1 = Option("Quiz1",[0,0,0], "")
-        choix_quiz2 = Option("Quiz2",[0,0,0], "")
-        choix_quiz3 = Option("Quiz3",[0,0,0], "")
-        menu_options([choix_quiz1,choix_quiz2,choix_quiz3])
-else:#On lance le mode 2 joueurs
+    else:#On lance le mode 2 joueurs
