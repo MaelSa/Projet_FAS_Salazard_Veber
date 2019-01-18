fdd620c5c8c4b32ce979fa609a512380d45de24a
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-01-18 21:31:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 from EcranLCD import *
 from Options import *
 from menu import *
-Mode_1_joueur = Option("Mode 1 joueur", [0,0,0], "")
-Mode_2_joueurs = Option("Mode 2 joueurs", [0,0,0], "")
+from Instances_classes import *
+Mode_1_joueur = Option("Mode 1 joueur", [255,0,0], "")
+Mode_2_joueurs = Option("Mode 2 joueurs", [0,255,0], "")
 fin_de_programme = False
 while not fin_de_programme:
     Option_choisie = menu_options([Mode_1_joueur, Mode_2_joueurs])
@@ -12,9 +13,9 @@ while not fin_de_programme:
     if Option_choisie == 1:#On lance le mode 1 joueur
         changer_mode = False
         while not changer_mode:
-            choix_quiz1 = Option("Quiz1",[0,0,0], "")
-            choix_quiz2 = Option("Quiz2",[0,0,0], "")
-            choix_quiz3 = Option("Quiz3",[0,0,0], "")
+            choix_quiz1 = Option("Quiz1",[255,0,0], "")
+            choix_quiz2 = Option("Quiz2",[0,255,0], "")
+            choix_quiz3 = Option("Quiz3",[0,0,255], "")
             quiz_choisi = menu_options([choix_quiz1,choix_quiz2,choix_quiz3])
             if quiz_choisi == 1:
                 score = quiz1.executer_quiz()
