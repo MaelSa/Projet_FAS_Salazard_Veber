c50127f8b21616038fadb96782a2f4f89c83ae0a
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-01-19 21:27:26 +0100

    Instance claszses création option

--- Instances_classes.py
+++ Instances_classes.py
@@ -4,14 +4,16 @@ from Question import *
 from Options import *
 from Quiz import *
 
-option1 = Option()
+option1 = Option("Premiere option", [0, 255, 255], "")
+option2 = Option("Seconde option", [0, 100, 50], "")
+option3 = Option("Troisieme option", [40,50,100], "")
 
 Question_1 = QuestionAssist("Trouver le serpent", (100, 20, 100), 2, 5)
 #Réponse : en or, en dessous du pot qui fume
 
 Question_3 = QuestionsINT("Combien de personnages", 15, 0, 40)
 
-
+Question_2 = QuestionQCM("Question qcm",[option1, option2, option3], 1)
 
 Question_8 = QuestionAssist("Trouver la lance/marteau", (0, 0, 0), 0, 0)
 #Réponse, vers la droite
@@ -46,6 +48,6 @@ Question_8 = QuestionAssist("Trouver la lance/marteau",(0,0,0),0,0)
 
 #Question_13 = QuestionQCM()
 
-quiz1 = Quiz([Question_1, Question_3, Question_8],"Debut du quiz 1","fin du quiz 1",2)
+quiz1 = Quiz([Question_1, Question_3, Question_2],"Debut du quiz 1","fin du quiz 1",2)
 quiz2 = Quiz([Question_1, Question_3, Question_8],"Debut du quiz 1","fin du quiz 1",2)
 quiz3 = Quiz([Question_1, Question_3, Question_8],"Debut du quiz 1","fin du quiz 1",2)
