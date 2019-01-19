f403029479b3eb3be134c9f688b02a10e9297b2c
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-01-19 19:57:19 +0100

    erreur menu

--- EcranLCD.py
+++ EcranLCD.py
@@ -125,10 +125,11 @@ def menu_options(Options):
     	num_current_option = len(Options) - 1
     quit = False
     while not quit:
+        print(num_current_option)
         action_potentiometre, action_bouton1, action_bouton2 = afficherLCD(Options[num_current_option].name, Options[num_current_option].color, len(Options))
         #print(action_potentiometre)
-        print(num_current_option)
-        if action_potentiometre == 1 and num_current_option != len(Options) - 1:
+
+        if action_potentiometre == 1 and num_current_option < len(Options) - 1:
             num_current_option += 1
         elif action_potentiometre == -1 and num_current_option > 0:
             num_current_option -= 1
