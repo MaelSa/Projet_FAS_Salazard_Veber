3d8aa5305f2e5dd73a4079b6d110da61608700d6
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-01-20 21:45:13 +0100

    try

--- Mode_2_joueurs.py
+++ Mode_2_joueurs.py
@@ -10,7 +10,7 @@ hote = "192.168.1.53"
 port = 8888
 
 def executer_mode_2():
-    
+
     changer_mode = False
     while not changer_mode:
         print("on entre dans le menu des quizz")
--- server.py
+++ server.py
@@ -5,13 +5,15 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(('', 8888))
 
 while True:
-        socket.listen(1)
-        client, address = socket.accept()
-        print("{} connected".format( address ))
+        socket.listen(5)
+        while true:
 
-        response = client.recv(255)
-        if response != "":
-                print(response)
+                client, address = socket.accept()
+                print("{} connected".format( address ))
+
+                response = client.recv(255)
+                if response != "":
+                        print(response)
 
 print("Close")
 client.close()
