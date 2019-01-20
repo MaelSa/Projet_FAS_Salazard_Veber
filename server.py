e823e19d70fc5db10863486ed94f2f244eb646d1
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-01-20 22:26:57 +0100

    serv

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(('', 8888))
 finfin = False
+c = 0
 while not finfin:
         socket.listen(1)
         client, address = socket.accept()
@@ -12,9 +13,11 @@ while not finfin:
             response = client.recv(255)
             if response != "b''":
                 print(response)
-                if response == "fin":
-                        fin = True
-                        finfin = True
+                c += 1
+                if c > 3:
+                    fin = True
+                    finfin = True
+
 
 print("Close")
 client.close()
