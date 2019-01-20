0f238a25a489e9abf917fab1f057e39dccf12c1f
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-01-20 22:56:25 +0100

    serv

--- server.py
+++ server.py
@@ -1,29 +1,29 @@
 
 import socket
-
+import time
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(('', 8888))
 finfin = False
 c = 0
 print("ok")
 while not finfin:
-        socket.listen(1)
-        print("ok")
+        socket.listen(5)
+        print("Serveur lancé")
         client, address = socket.accept()
-        print("ok")
+
         fin = False
+        c = 0
         while not fin:
-            print("ok2")
+            print("Serveur connecté")
             response = client.recv(255)
             if response != "b''":
                 print(response)
-                #c += 1
-                #print(c)
-                #if c > 3:
-                #    fin = True
-                #    finfin = True
+                c += 1
+                if c > 3:
+                    fin = True
+                    finfin = True
 
-
+time.sleep(10)
 print("Close")
 client.close()
 stock.close()
