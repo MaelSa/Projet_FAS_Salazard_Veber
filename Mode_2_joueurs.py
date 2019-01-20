3e311e61b5ee55e86c770b9af61947d44c8f1c1f
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-01-20 21:20:51 +0100

    import bug

--- Mode_2_joueurs.py
+++ Mode_2_joueurs.py
@@ -10,6 +10,7 @@ hote = "192.168.1.53"
 port = 8888
 
 def executer_mode_2():
+    import socket
     hote = "192.168.1.53"
     port = 8888
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
