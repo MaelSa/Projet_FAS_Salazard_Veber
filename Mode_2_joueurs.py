602be187089ee989db89dc9a737510ef14b891ae
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-02-14 18:05:29 +0100

    seg

--- Mode_2_joueurs.py
+++ Mode_2_joueurs.py
@@ -6,7 +6,7 @@ def main_2j(num_client):
     print("on lance le mode deux joueurs main2j")
     port = [12800, 5000]
     print("on commence")
-    t1 = threading.Thread( target = client_2j, args = (port[num_client], "192.168.1.53", ) )
+    #t1 = threading.Thread( target = client_2j, args = (port[num_client], "192.168.1.53", ) )
     #t1.start()
     print("premier thread")
     t2 = threading.Thread( target  = executer_mode_2, args = (num_client, ) )
